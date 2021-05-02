b60dc8e2ee5a1306670e434fe96dbb25d3b42698
pkunzipula <jcapra76@gmail.com>
2021-05-02 15:31:26 -0700

    Update addTransactions.js

--- src/containers/transactions/addTransactions.js
+++ src/containers/transactions/addTransactions.js
@@ -38,6 +38,50 @@ const AddTransactions = ({ stateProperties, addTransaction }) => {
     };
   });
 
+  const paymentTypes = [
+    { label: "Expense", value: "Expense", color: "white" },
+    { label: "Payment", value: "Payment", color: "white" },
+  ];
+
+  const transactionCategories = [
+    { label: "Appraisal", value: "Appraisal", color: "white" },
+    { label: "Cleaning", value: "Cleaning", color: "white" },
+    { label: "Inspection", value: "Inspection", color: "white" },
+    { label: "Marketing", value: "Marketing", color: "white" },
+    {
+      label: "Renovations",
+      value: "Renovations",
+      color: "white",
+    },
+    {
+      label: "Rent Payment",
+      value: "Rent Payment",
+      color: "white",
+    },
+    { label: "Repairs", value: "Repairs", color: "white" },
+    {
+      label: "Security Deposit",
+      value: "Secuirty Deposit",
+      color: "white",
+    },
+    {
+      label: "Tax Services",
+      value: "Tax Services",
+      color: "white",
+    },
+  ];
+
+  const paymentMethods = [
+    {
+      label: "Bank Transfer",
+      value: "Bank Transfer",
+      color: "white",
+    },
+    { label: "Cash", value: "Cash", color: "white" },
+    { label: "Check", value: "Check", color: "white" },
+    { label: "Other", value: "Other", color: "white" },
+  ];
+
   const addItem = (data) => {
     data.date = date.toString();
     addTransaction(data);
@@ -166,10 +210,7 @@ const AddTransactions = ({ stateProperties, addTransaction }) => {
                 placeholder={TransactionPlaceholder}
                 style={pickerStyles}
                 onValueChange={(value) => onChange(value)}
-                items={[
-                  { label: "Expense", value: "Expense", color: "white" },
-                  { label: "Payment", value: "Payment", color: "white" },
-                ]}
+                items={paymentTypes}
               />
             )}
             name="payment"
@@ -197,33 +238,7 @@ const AddTransactions = ({ stateProperties, addTransaction }) => {
                 placeholder={CategoryPlaceholder}
                 style={pickerStyles}
                 onValueChange={onChange}
-                items={[
-                  { label: "Appraisal", value: "Appraisal", color: "white" },
-                  { label: "Cleaning", value: "Cleaning", color: "white" },
-                  { label: "Inspection", value: "Inspection", color: "white" },
-                  { label: "Marketing", value: "Marketing", color: "white" },
-                  {
-                    label: "Renovations",
-                    value: "Renovations",
-                    color: "white",
-                  },
-                  {
-                    label: "Rent Payment",
-                    value: "Rent Payment",
-                    color: "white",
-                  },
-                  { label: "Repairs", value: "Repairs", color: "white" },
-                  {
-                    label: "Security Deposit",
-                    value: "Secuirty Deposit",
-                    color: "white",
-                  },
-                  {
-                    label: "Tax Services",
-                    value: "Tax Services",
-                    color: "white",
-                  },
-                ]}
+                items={transactionCategories}
               />
             )}
             name="transactionCategory"
@@ -279,16 +294,7 @@ const AddTransactions = ({ stateProperties, addTransaction }) => {
                 placeholder={PaymentPlaceholder}
                 style={pickerStyles}
                 onValueChange={(value) => onChange(value)}
-                items={[
-                  {
-                    label: "Bank Transfer",
-                    value: "Bank Transfer",
-                    color: "white",
-                  },
-                  { label: "Cash", value: "Cash", color: "white" },
-                  { label: "Check", value: "Check", color: "white" },
-                  { label: "Other", value: "Other", color: "white" },
-                ]}
+                items={paymentMethods}
               />
             )}
             name="paymentMethod"
